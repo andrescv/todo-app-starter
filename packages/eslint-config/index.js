37f3963ed9a1881c15c8bab3cfe2b9eac065f679
Andrés Castellanos <me@andrescv.dev>
2022-07-26 16:49:53 -0600

    chore: update ESLint config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,11 +1,20 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'prettier'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   plugins: ['simple-import-sort'],
+  parserOptions: {
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
   rules: {
     'no-unused-vars': 'off',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
     '@next/next/no-html-link-for-pages': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
   overrides: [
     {
